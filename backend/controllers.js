7348ce1cf3e6d7e09cc643be396d2745ea1b66c4
Richard Shaju <richardshaju66@gmail.com>
2024-05-03 18:13:53 +0530

    tweaks in prompt

--- backend/controllers.js
+++ backend/controllers.js
@@ -99,6 +99,8 @@ VASA goes beyond simple lip-syncing. It captures a wide range of facial expressi
 
 *VASA:* https://www.microsoft.com/en-us/research/project/vasa-1/
 
+(the given below lines should show exactly as same)
+
 Feel free to ask doubts here or whenever you see us! 😉
 
 See you next Wednesday!
