70349826c9d2fb9685777e8220b25ce75fbee5d1
richardshaju <richardshaju66@gmail.com>
2025-04-29 22:40:45 +0530

    fix: correct environment variable name for MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.post("/generate", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_UR || "mongodb://localhost:27017/legacytimes", {
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/legacytimes", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
